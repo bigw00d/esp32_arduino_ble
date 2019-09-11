727c974c4acfc81c8f03ce86679fd9c288e09fe5
Daiki <daiking_tensai@yahoo.co.jp>
2019-09-11 09:41:36 +0900

    update demo

--- examples/serial_ble_converter/README.md
+++ examples/serial_ble_converter/README.md
@@ -3,20 +3,31 @@
 Converter from serial UART to Bluetooth LE UART
 
 ## Requirement
-
+- M5Stick-C or something else esp32 device
 - Arduino Version: 1.8.5
+- Arduino board manager ( https://dl.espressif.com/dl/package_esp32_index.json )
 
 
-## Hardware Connections
 
-|M5Stick-C (UART Receiver)  |ESP32 (UART Transmitter)  |
-|---|---|
-|IO26  |IO27  |
-|GND  |GND  |
-
-## Task information in M5Stick-C application
+## Task list in this application
 
 |   |Core   |Job  |
 |---|---|---|
 |MainTask (main loop)  |core 0  |UART Receiver |
 |Task0 |core 0  |BLE Transmitter |
+
+## Demo
+The demo send uart data from another ESP32 sample application( ./tools/esp32_uart_transmitter/ ) to this application. 
+With Application [nRF UART 2.0](https://play.google.com/store/apps/details?id=com.nordicsemi.nrfUARTv2&hl=ja), you can view BLE UART data.
+
+#### Hardware Connections
+
+|M5Stick-C (UART Receiver)  |ESP32 (UART Transmitter)  |
+|---|---|
+|IO26  |IO27  |
+|GND  |GND  |
+　
+
+#### BLE UART data
+<img src="./Demo.png" />
+　
--- examples/serial_ble_converter/tools/esp32_uart_transmitter/esp32_uart_transmitter.ino
+++ examples/serial_ble_converter/tools/esp32_uart_transmitter/esp32_uart_transmitter.ino
@@ -1,5 +1,15 @@
-#include <M5StickC.h>
- 
+//  note:
+//    use Arduino 1.8.5
+//    Arduino board manager
+//      https://dl.espressif.com/dl/package_esp32_index.json
+//    ボード ESP32 Dev Module
+//    Flash Mode  QIO
+//    Flash Frequency 80MHz
+//    Flash Size  4M (32Mb)
+//    Partition Scheme  No OTA (2MB APP/2MB FATFS)
+//    Upload Speed  115200
+//    Core Debug Level なし
+
 void setup() {
   Serial.begin(115200); // Debug Print
   Serial2.begin(115200, SERIAL_8N1, 26, 27); //rx:G26, tx:G27
@@ -7,28 +17,7 @@ void setup() {
  
 void loop() {
   static int sendCount = 0;
-  Serial2.println("I/ start sending");
+  Serial2.printf("send %d\r\n", sendCount);
   delay(500);
-  Serial2.println("1, SWVer, M5StickUARTV001");
-  delay(500);
-  Serial2.println("2, State, Sending");
-  delay(500);
-  Serial2.printf("3, Count1, %d\r\n", sendCount);
-  delay(500);
-  Serial2.printf("4, Count2, %d\r\n", sendCount);
-  delay(500);
-  Serial2.printf("6, VDD, %d\r\n", (sendCount)%60);
-  delay(500);
-  sendCount++;
-  Serial2.println("I/ wait response");
-  delay(500);
-  Serial2.println("1, SWVer, M5StickUARTV001");
-  delay(500);
-  Serial2.println("2, State, Receiving");
-  delay(500);
-  Serial2.printf("5, Count3, %d\r\n", sendCount);
-  delay(500);
-  Serial2.printf("6, VDD, %d\r\n", (sendCount+20)%60);
-  delay(500);
-  sendCount++;
+  sendCount = (sendCount+1) % 100;
 }
